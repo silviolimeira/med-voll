1ab37fdcadfbad30ccf8c7af084296717d5e5d5a
Silvio Limeira <sicemal@gmail.com>
2023-05-14 12:09:50 -0300

    Add get Listagem de medicos

--- src/main/java/com/sl/med/voll/api/controller/MedicoController.java
+++ src/main/java/com/sl/med/voll/api/controller/MedicoController.java
@@ -1,12 +1,16 @@
 package com.sl.med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sl.med.voll.api.medico.DadosCadastroMedico;
+import com.sl.med.voll.api.medico.DadosListagemMedico;
 import com.sl.med.voll.api.medico.Medico;
 import com.sl.med.voll.api.medico.MedicoRepository;
 
@@ -26,6 +30,11 @@ public class MedicoController {
 	@Transactional
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
+	}
+	
+	@GetMapping
+	public List<DadosListagemMedico> listar() {
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
 	}
 
 }
